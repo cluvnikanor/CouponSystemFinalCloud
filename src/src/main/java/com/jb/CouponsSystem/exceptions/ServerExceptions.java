1bd13948e24a5b3afc753002dc2c55b678114950
cluvnikanor <cluvnikanor@gmail.com>
2020-10-21 21:24:14 +0300

    update

---
+++ src/src/main/java/com/jb/CouponsSystem/exceptions/ServerExceptions.java
@@ -0,0 +1,10 @@
+package com.jb.CouponsSystem.exceptions;
+
+@SuppressWarnings("serial")
+public class ServerExceptions extends RuntimeException {
+
+	public ServerExceptions(String message) {
+		super(message);
+	}
+
+}
